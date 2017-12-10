513365baa1b0ac56224c9cb5cd9f852b56e4dacd
StefanSohnchen <32264182+StefanSohnchen@users.noreply.github.com>
2017-12-10 13:22:53 +1300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,6 +1,8 @@
-data<- read.table("household_power_consumption.txt", sep=";", header = T,
-                  stringsAsFactors = FALSE, dec = ".")
+# download dataset to working directory and the read the file into RStudio
 
+data<- read.table("household_power_consumption.txt", sep=";", header = T, stringsAsFactors = FALSE, dec = ".")
+
+# subset the required data
 
 data<- subset(data, data$Date %in% c("1/2/2007","2/2/2007"))
 
@@ -12,6 +14,8 @@ data$Sub_metering_1<- as.numeric(data$Sub_metering_1)
 data$Sub_metering_2<- as.numeric(data$Sub_metering_2)
 data$Sub_metering_3<- as.numeric(data$Sub_metering_3)
 
+# save the file as "plot3.png" in working directory and create plot
+
 png("plot3.png", width = 480, height = 480)
 with(data, {
   plot(datetime,data$Sub_metering_1, type="l",
@@ -21,4 +25,7 @@ with(data, {
 })
 legend("topright", col=c("black", "red", "blue"), 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = 1, lwd = 2)
+
+# dev is closed
+
 dev.off()
